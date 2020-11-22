2ff8f6827be8833cff16a14b253733173ee80d5e
Ninedeadeyes <tommykwong_84@hotmail.com>
2020-11-22 11:03:18 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -1,17 +1,25 @@
 from TankBattlePVE import*
 from TankBattlePVP import*
 
+option=False
 
 def game():
-    print("Welcome to Tank Battle at Ghost Forest")
-    choice=input ("1 or 2 player ? ")
-    if choice== "1":
-        input("1 player mode it is. (Press enter to continue)")
-        PVE()
+    while option==False:
+        print("Welcome to Tank Battle at Ghost Forest")
+        choice=input ("1 or 2 player ? ")
+        if choice== "1":
+            input("1 player mode it is. (Press enter to continue)")
+            PVE()
+            break 
 
-    else:
-        input("2 player mode it is. (Press enter to continue)")
-        PVP()
+        if choice== "2":
+            input("2 player mode it is. (Press enter to continue)")
+            PVP()
+            break
+
+        else:
+            pass 
+            
 
 
 game()
